303fb65ea7756a243b0feb82f7c43faf4cb31fd3
mohehexd <dumblemoe@gmail.com>
2020-12-18 17:14:43 -0600

    Creating a index method in players controller.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,19 @@
 class BoardsController < ApplicationController
+    
+    def index
+        render json: Board.all 
+    end
+    
     def create
-        byebug
+        board = Board.new(board_params)
+
+        if board.save
+            render json: board
+        end
+    end
+
+    private
+    def board_params
+        params.require(:board).permit(:score)
     end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,11 @@
 class PlayersController < ApplicationController
+
+    def index
+        render json: Player.all
+    end
+
+    def create
+        byebug  
+        # render json: Player.create
+    end
 end
