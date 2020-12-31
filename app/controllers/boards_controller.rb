2ab723000e7fb0c1e46c7dadf6f015afc54873b9
mohehexd <dumblemoe@gmail.com>
2020-12-31 17:03:13 -0600

    Updating boards serializer.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,12 +2,14 @@ class BoardsController < ApplicationController
     
     def index
         render json: Board.all.map {|board| BoardSerializer.new(board)}
+        # render json: Board.all 
     end
     
     def create
         board = Board.new(board_params)
 
         if board.save
+            # render json: board
             render json: BoardSerializer.new(board)
         end
     end
