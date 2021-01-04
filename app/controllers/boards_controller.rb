b3ac1d60862e02e8c4b29aa41344a63b2ff0e6da
mohehexd <dumblemoe@gmail.com>
2021-01-03 22:07:24 -0600

    Removing board serializer from Boards controller to work on dataset id

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,16 +1,16 @@
 class BoardsController < ApplicationController
     
     def index
-        render json: Board.all.map {|board| BoardSerializer.new(board)}
-        # render json: Board.all 
+        # render json: Board.all.map {|board| BoardSerializer.new(board)}
+        render json: Board.all 
     end
     
     def create
         board = Board.new(board_params)
 
         if board.save
-            # render json: board
-            render json: BoardSerializer.new(board)
+            render json: board
+            # render json: BoardSerializer.new(board)
         end
     end
 
--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -2,3 +2,4 @@ class BoardSerializer
   include FastJsonapi::ObjectSerializer
   attributes :score, :players
 end
+
